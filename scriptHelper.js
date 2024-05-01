7baf67a5ce13b43f71f1dac57cbb1d61ef1b15c8
At0ned <sethaholt@gmail.com>
2024-05-01 18:47:41 -0500

    Task 2 completed  except for color on launchStatus

--- scriptHelper.js
+++ scriptHelper.js
@@ -79,30 +79,34 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`
 
     let fuelStatus = document.getElementById("fuelStatus");
-  
+    let cargoStatus = document.getElementById("cargoStatus");
+
     if (Number(fuelLevel) < 10000) {
         fuelStatus.innerHTML = `Fuel level too low for launch`;
         launchStatus.innerHTML = `Shuttle Not Ready for Launch`
         list.style.visibility = "visible";
     } else {
         fuelStatus.innerHTML = `Fuel level high enough for launch`;
-        launchStatus.innerHTML = `Shuttle is ready for launch`
         list.style.visibility = "visible";
-        
+
     }
     
-    let cargoStatus = document.getElementById("cargoStatus")
-
-    if (Number(cargoLevel) > 10000) { 
-        cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
+    if (Number(cargoLevel) > 10000) {
+        cargoStatus.innerHTML = `Cargo mass too heavy for launch`
         launchStatus.innerHTML = `Shuttle Not Ready for Launch`
         list.style.visibility = "visibile";
     } else {
-        cargoStatus.innerHTML = `Cargo mass low enough for launch`;
-        launchStatus.innerHTML = `Shuttle is ready for launch`
+        cargoStatus.innerHTML = `Cargo mass low enough for launch`
         list.style.visibility = "visibile";
     }
-
+        
+    
+    if (Number(fuelLevel) > 10000 && Number(cargoLevel) < 10000) {
+        launchStatus.innerHTML = `Shuttle is ready for launch`
+        list.style.visibility = "visible";
+    }
+  
+    
     
     // list.style.visibility = "visibile";
  
