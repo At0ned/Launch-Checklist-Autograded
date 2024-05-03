f7b14469ca38e8d3fd6604b0dc1dafb7b863610a
At0ned <sethaholt@gmail.com>
2024-05-03 11:08:07 -0500

    Complete

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,36 +1,7 @@
 // Write your helper functions here!
 
 require('cross-fetch/polyfill');
-/*Make an alert all fields are required
 
-scriptHelper => validateInput()
-Check valid info was passed in for each field > easily converted to the correct data type
--the pilot and co-pilot names should be strings
--fuel level and cargo mass should be numbers
--should take in string as a parameter and return "Empty", "Not a Number" or "is a Number" as valid.
-
-use validateInput() to complete formSubmission()
-
-formSubmission()
--takes a document as a parameter, and strings representing the pilot, co-pilot, fuel level, and cargo mass
--the document parameter for your HTML document, update the shuttle requirements.
--make the call for formSubmission at the right time in script.js.
-
-Update shuttle requirements
--the div and id faultyitems should be updated if something is not ready for launch
--using template literals, update the li elements pilotStatus and copilotStatus to include their names
-
--If the user submits a fuel level that is too low (less than 10,000 liters), change faultyItems to visible 
-with an updated fuel status stating that there is not enough fuel for the journey. 
-The text of the h2 element, launchStatus, should also change to “Shuttle not ready for launch” and the color should change to red.
-
--If the user submits a cargo mass that is too large (more than 10,000 kilograms), 
-change the list to visible with an updated cargo status stating that there is too much mass for the shuttle to take off. 
-The text of launchStatus should also change to “Shuttle not ready for launch” and the color should change to red.
-
--If the shuttle is ready to launch, change the text of launchStatus to green and display “Shuttle is ready for launch”.
-
-*/
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
@@ -65,10 +36,13 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel)  === "Empty" || validateInput(cargoLevel)  === "Empty") {
         window.alert("All fields are required");
+        return
     } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
         window.alert("Enter in a valid input.")
+        return
     } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
         window.alert("Enter in a valid input.")
+        return
     } 
 
     let launchStatus = document.getElementById("launchStatus")
